6e369752fa332fb355df784814ed28c81104fd56
newraina <newraina@qq.com>
2016-02-19 21:38:36 +0800

    稍作改进

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 function meTemplate(tpl, data) {
-    var re = /{%[\s?]+([^&}]+)?[\s?]+%}/g;
-    var result = tpl.replace(re, function (p0, p1) {
-        return data[p1];
-    });
-    return result;
+    var re     = /{%[\s?]+([^&}]+)?[\s?]+%}/g;
+    var code   = 'var r=[];\n';
+    var cursor = 0;
+    var match;
+
+    function add(line, js) {
+        code += js ? 'r.push(' + line.replace(/"/g, '\"') + ');\n' : 'r.push("' + line.replace(/"/g, '\"') + '");\n';
+    }
+
+    while (match = re.exec(tpl)) {
+        add(tpl.slice(cursor, match.index));
+        add(match[1], true);
+        cursor = match.index + match[0].length;
+    }
+    add(tpl.slice(cursor));
+    code += 'return r.join("");';
+
+    return (new Function(code.replace(/[\r\t\n]/g, '')).bind(data))();
+
 }
 
-var tpl = '<p>my name is {% name %}, i\'m {% age %} years old.</p>';
+var tpl = '<p>my name is {% this.name %}, i\'m {% this.age %} years old.</p>';
 
 console.log(meTemplate(tpl, {name: 'Tom', age: 18}));
